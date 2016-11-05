272e3f61ff6ad47a6eef60ad70989908f80fd6cf
Matthew Muccio <me@matthewmuccio.com>
2016-11-05 08:07:34 -0400

    Very small edits to ViewController; made color BarStyle of navigationBar black.

--- QuotaiOS/ViewController.swift
+++ QuotaiOS/ViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidLoad()
+    {
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.black;
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
